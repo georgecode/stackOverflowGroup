4ecdd702c08a70e681a793706fb3212532451bbd
georgecode <georgeczurlanis@gmail.com>
2017-02-02 15:24:48 -0800

    Add header partial user controller user views

---
+++ app/controllers/users.rb
@@ -0,0 +1,23 @@
+# get '/users' do
+# end
+
+get'/users/new' do
+  erb :'users/new'
+end
+
+# post '/users' do
+# end
+
+# get '/users/:id' do
+# end
+
+# get '/users/:id/edit' do
+# end
+
+# put '/users/:id' do
+# end
+
+# delete '/users/:id' do
+# end
+
+
