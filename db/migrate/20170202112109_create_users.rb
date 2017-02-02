1534eafae735e1a8e419660ef405252485a090a0
slinkous <sralinkous@gmail.com>
2017-02-02 13:52:21 -0800

    Fix migrations typo

--- db/migrate/20170202112109_create_users.rb
+++ db/migrate/20170202112109_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :email
       t.string :hashed_password
 
-      t.timestamps (null: false)
+      t.timestamps(null: false)
     end
   end
 end
